4ad821754969d553be18f91adf27e13bdde9d808
Artur Witkowski <a.witkowski99@gmail.com>
2022-06-07 01:50:03 +0200

    finished

--- stegano.py
+++ stegano.py
@@ -1,6 +1,10 @@
 import sys
 import re
 
+if len(sys.argv) < 3:
+    print('Usage: python stegano.py <-e/-d> <-1/-2/-3/-4>')
+    exit(1)
+
 option = sys.argv[1]
 algorithm = sys.argv[2]
 
@@ -36,7 +40,6 @@ def encodeEndlineSpaces():
             return
 
         for i in range(len(binToEncode)):
-            htmlSource[i]
             if (binToEncode[i] == '1'):
                 htmlSource[i] = htmlSource[i][:-1].rstrip() + f' \n'
             else:
@@ -85,18 +88,206 @@ def encodeDoubleSpaces():
             while re.search(r'[ ]{2,}', htmlSource[i]):
                 htmlSource[i] = re.sub(r'[ ]{2,}', ' ', htmlSource[i])
 
-        # TODO: create good replacement system for single spaces
         line = 0
+        startIndex = 0
         for i in range(len(binToEncode)):
-            while not re.search(r'[^ ][ ]{1}[^ ]', htmlSource[line]):
+            foundPlace = None
+            while foundPlace == None:
+                occ = [(m.start(0), m.end(0)) for m in re.finditer(r'^[ ]{1}[^ ]|[^ ][ ]{1}$|[^ ][ ]{1}[^ ]', htmlSource[line])]
+                for j in range(len(occ)):
+                    if occ[j][0] >= startIndex:
+                        foundPlace = occ[j]
+                        break
+                if foundPlace != None:
+                    break
                 line += 1
+                startIndex = 0
+            [start, end] = foundPlace
             if (binToEncode[i] == '1'):
-                htmlSource[line] = re.sub(r'([^ ])[ ]{1}([^ ]', '  ', htmlSource[line])
+                if start > 0: start = start + 1
+                if end < len(htmlSource[line])-1: end = end - 1
+                htmlSource[line] = htmlSource[line][:start] + '  ' + htmlSource[line][end:]
+                startIndex += 1
+            startIndex = foundPlace[0] + 1
+                
         with open('watermark.html', 'w') as f:
             f.write("".join(htmlSource))
 
 def decodeDoubleSpaces():
-    pass
+    with open('watermark.html') as f:
+        htmlSource = f.readlines()
+        message = ''
+        for i in range(len(htmlSource)):
+            occ = [(m.start(0), m.end(0)) for m in re.finditer(r'[ ]+', htmlSource[i])]
+            for [start, end] in occ:
+                if end - start == 2:
+                    message += '1'
+                else:
+                    message += '0'
+                
+        diff = len(message) % 4 + 1
+        if diff != 0:
+            message = message[:-(diff)]
+        while message[-4:] == '0000':
+            message = message[:-4]
+
+        hexMessage = binToHex(message)
+        with open('detect.txt', 'w') as f:
+            f.write(hexMessage)
+            f.write('\n')
+            f.write(hexToText(hexMessage))
+
+def encodeWrongCSS():
+    message = None
+    with open('mess.txt') as f:
+        message = f.readlines()[0]
+    with open('cover.html') as f:
+        htmlSource = f.readlines()
+        binToEncode = hexToBin(message)
+        tagRegex = r'<[a-zA-Z]+>'
+        wrongCSS = ' style="margin-botom: 0cm; lineheight: 100%"'
+
+        counterTages = 0
+        for i in range(len(htmlSource)):
+            occ = [(m.start(0), m.end(0)) for m in re.finditer(tagRegex, htmlSource[i])]
+            counterTages += len(occ)
+
+        if counterTages < len(binToEncode):
+            print('Message is too long to encode')
+            return
+
+        line = 0
+        startIndex = 0
+        for i in range(len(binToEncode)):
+            foundPlace = None
+            while foundPlace == None:
+                occ = [(m.start(0), m.end(0)) for m in re.finditer(tagRegex, htmlSource[line])]
+                for j in range(len(occ)):
+                    if occ[j][0] >= startIndex:
+                        foundPlace = occ[j]
+                        break
+                if foundPlace != None:
+                    break
+                line += 1
+                startIndex = 0
+            [start, end] = foundPlace
+            if (binToEncode[i] == '1'):
+                htmlSource[line] = htmlSource[line][:end-1] + wrongCSS + htmlSource[line][end-1:]
+            startIndex = start + 1
+        
+        with open('watermark.html', 'w') as f:
+            f.write("".join(htmlSource))
+    
+def decodeWrongCSS():
+    with open('watermark.html') as f:
+        htmlSource = f.readlines()
+        tagRegex = r'<[a-zA-Z]+>'
+        wrongCSS = ' style="margin-botom: 0cm; lineheight: 100%"'
+        message = ''
+
+        for i in range(len(htmlSource)):
+            occTag = [m.start(0) for m in re.finditer(tagRegex, htmlSource[i])]
+            occWrong = [m.start(0) for m in re.finditer(wrongCSS, htmlSource[i])]
+            maxTag, maxWrong = 0, 0
+            if len(occTag) > 0:
+                maxTag = occTag[-1]
+            if len(occWrong) > 0:
+                maxWrong = occWrong[-1]
+            for j in range(max(maxTag, maxWrong)+1):
+                if j in occTag:
+                    message += '0'
+                elif j in occWrong:
+                    message += '1'
+                
+        diff = len(message) % 4 + 1
+        if diff != 0:
+            message = message[:-(diff)]
+        while message[-4:] == '0000':
+            message = message[:-4]
+
+        hexMessage = binToHex(message)
+        with open('detect.txt', 'w') as f:
+            f.write(hexMessage)
+            f.write('\n')
+            f.write(hexToText(hexMessage))
+
+def encodeEmptyTags():
+    message = None
+    with open('mess.txt') as f:
+        message = f.readlines()[0]
+    with open('cover.html') as f:
+        htmlSource = f.readlines()
+        binToEncode = hexToBin(message)
+        tagOpen = '<font>'
+        tagEnd = '</font>'
+        tagOpenFiller = '<font></font><font>'
+        tagEndFiller = '</font><font></font>'
+
+        counterTages = 0
+        for i in range(len(htmlSource)):
+            occ = [(m.start(0), m.end(0)) for m in re.finditer(tagOpen, htmlSource[i])]
+            counterTages += len(occ)
+
+        if counterTages < len(binToEncode):
+            print('Message is too long to encode')
+            return
+
+        line = 0
+        startIndex = 0
+        for i in range(len(binToEncode)):
+            foundPlace = None
+            while foundPlace == None:
+                occ = []
+                if binToEncode[i] == '1':
+                    occ = [(m.start(0), m.end(0)) for m in re.finditer(tagOpen, htmlSource[line])]
+                else:
+                    occ = [(m.start(0), m.end(0)) for m in re.finditer(tagEnd, htmlSource[line])]
+                for j in range(len(occ)):
+                    if occ[j][0] >= startIndex:
+                        foundPlace = occ[j]
+                        break
+                if foundPlace != None:
+                    break
+                line += 1
+                startIndex = 0
+            [start, end] = foundPlace
+            if (binToEncode[i] == '1'):
+                htmlSource[line] = htmlSource[line][:start] + tagOpenFiller + htmlSource[line][end:]
+            else:
+                htmlSource[line] = htmlSource[line][:start] + tagEndFiller + htmlSource[line][end:]
+            line += 1
+            startIndex = 0
+        
+        with open('watermark.html', 'w') as f:
+            f.write("".join(htmlSource))
+    
+def decodeEmptyTags():
+    with open('watermark.html') as f:
+        htmlSource = f.readlines()
+        tagOpenFiller = '<font></font><font>'
+        tagEndFiller = '</font><font></font>'
+        message = ''
+
+        for i in range(len(htmlSource)):
+            occOpen = [m.start(0) for m in re.finditer(tagOpenFiller, htmlSource[i])]
+            occEnd = [m.start(0) for m in re.finditer(tagEndFiller, htmlSource[i])]
+            maxOpen, maxEnd = 0, 0
+            if len(occOpen) > 0:
+                maxOpen = occOpen[-1]
+            if len(occEnd) > 0:
+                maxEnd = occEnd[-1]
+            for j in range(max(maxOpen, maxEnd)+1):
+                if j in occOpen:
+                    message += '1'
+                elif j in occEnd:
+                    message += '0'
+
+        hexMessage = binToHex(message)
+        with open('detect.txt', 'w') as f:
+            f.write(hexMessage)
+            f.write('\n')
+            f.write(hexToText(hexMessage))
+
 
 if option == '-e':
     if algorithm == '-1':
@@ -104,9 +295,9 @@ if option == '-e':
     elif algorithm == '-2':
         encodeDoubleSpaces()
     elif algorithm == '-3':
-        pass
+        encodeWrongCSS()
     elif algorithm == '-4':
-        pass
+        encodeEmptyTags()
     else:
         print('Wrong algorithm. Choose 1, 2, 3 or 4.')
     pass
@@ -116,9 +307,9 @@ elif option == '-d':
     elif algorithm == '-2':
         decodeDoubleSpaces()
     elif algorithm == '-3':
-        pass
+        decodeWrongCSS()
     elif algorithm == '-4':
-        pass
+        decodeEmptyTags()
     else:
         print('Wrong algorithm. Choose 1, 2, 3 or 4.')
     pass
